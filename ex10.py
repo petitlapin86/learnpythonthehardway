3fc04c99a68132b71e1d4ad4c0204f4fa43c6a8c
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-13 13:49:55 -0400

    excape clauses

---
+++ ex10.py
@@ -0,0 +1,21 @@
+#excape clauses
+
+
+tabby_cat = "\ti am tabbed in."
+persian_cat = "I'm split\non a line"
+
+#putting two \\ creates a single \ in the string 
+backslash_cat = "Im \\ a \\ cat"
+
+#triple quotes allows for a multi line string
+fat_cat = """
+I'll do a list:
+\t* cat Food
+\t* fishies
+\t* catnip\n\t* grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
