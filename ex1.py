ec67482e39fe0bce620f09e29f2ca9f0c55182c6
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-13 09:55:11 -0400

    test

--- ex1.py
+++ ex1.py
@@ -1,6 +1,10 @@
+# this is a comment in python
+
 print("hello, world!")
 print("Hello again")
 print("I like typing this")
 print("This is fun.")
 print("Yay! Printing")
 print("This is easy peasy")
+print("But i know it will get hard")
+print("so very very hard")
---
+++ ex2.py
@@ -0,0 +1,4 @@
+#exercise two
+
+def hello:
+  
