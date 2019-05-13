d8ceb237b319f33c7822bb718be6ce4192077fc6
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-13 13:39:34 -0400

    exercise 9 in learn ruby the hard way

---
+++ ex9.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thur Fri Sat Sun"
+months = "\nJan\nfeb\nmar\napr\nmay\njun\njul\naug"
+
+print("here are the days ", days)
+print("here are the months", months)
+
+print("""
+theres something going on here
+with the three double quotes
+we'll be able to type on multiple lines
+""")
