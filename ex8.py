1f62feb3b32db9eb29e09e3a13d74321dfb187f0
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-13 13:28:49 -0400

    formatting tricks

---
+++ ex8.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1,2,3,4))
+print(formatter.format("one","two","three","four"))
+print(formatter.format(True,False,True,True))
+print(formatter.format(formatter,formatter,formatter,formatter))
+print(formatter.format(
+"once upon a time",
+"a girl had a dream",
+"that she could learn to code",
+"maybe someday it will be true"
+))
