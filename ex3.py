124e95cb2726dc50651a336a7245d66d0a2310f4
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-13 10:14:59 -0400

    math operators

---
+++ ex3.py
@@ -0,0 +1,38 @@
+#exercise 3
+#numbers and math
+
+print("I will now count my chickens:")
+print("hens", 25 + 30 / 6)
+print("roosters", 100 - 25 * 3 % 4)
+
+print("now I will count the eggs:")
+
+# no quotes for math functions
+print(3 + 2 + 1 - 5 + 4  % 2 - 1 / 4 + 6 )
+
+#if you put the math into quotes it will become part of the string
+print("is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7)
+
+#if you want to do both in one line, format like below.
+print("what is 3 + 2?", 3 + 2)
+print("what is 5 - 7?", 5 - 7)
+
+print("Oh, thats why it is false")
+
+print("How about some more")
+
+print("Is it greater or equal?", 5 >= -2)
+print("is it less or equal?", 5<= -2)
+
+
+# + plus
+# - minus
+# / divide
+# % modulus
+# * multiply
+# > greater than
+# >= greater than or equal
+# < less than
+# <= less than or equal
