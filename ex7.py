4d7a594cff201b2267f6ba9669c9118517feb44f
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-13 13:06:49 -0400

    concatenation

---
+++ ex7.py
@@ -0,0 +1,21 @@
+print("Mary had a little lamb")
+print("Its fleece was white as {}.".format('snow')) #here im just inputting a string snow, its not a variable
+print("and everywhere that Mary went")
+print("." * 10) #prints ..........
+
+end1 = 'c'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'b'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+#below is an example of concatenation.
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12, end=' ')
