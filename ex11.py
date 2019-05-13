7e36b73653f6ce91e683f9727c1fbfa3417a074d
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-13 14:04:23 -0400

    input from user

---
+++ ex11.py
@@ -0,0 +1,10 @@
+#taking input from user
+print("how old are you?", end= ' ')
+age = input()
+print("how tall are you?", end= ' ')
+height = input()
+print("How much do you weigh?", end= ' ')
+weight = input()
+# end= ' ' tells print not to end the line with a newline character and go to the next line
+
+print(f"so you are {age} old, {height} tall, and {weight} pounds.")
