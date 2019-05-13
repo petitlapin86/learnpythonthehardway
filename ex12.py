685c815ec1dd29c2fff1ac63b2020269b4d1701a
petitlapin86 <skinnerpa86@yahoo.com>
2019-05-13 14:10:03 -0400

    storing input into variables

---
+++ ex12.py
@@ -0,0 +1,8 @@
+#prompts
+
+#this asks for input, creates a variable and stores the users response inside it
+age = input('how old are you?')
+weight = input('how much do you weigh?')
+height = input('how tall are you?')
+
+print(f"so you are {age} old {weight} pounds and {height} inches tall.")
