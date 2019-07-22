d1d511f019f291efefa6300bf73acb31aaa77e6b
petitlapin86 <skinnerpa86@yahoo.com>
2019-07-22 17:46:02 -0400

    complete 25

--- ex25.py
+++ ex25.py
@@ -17,3 +17,20 @@ def print_last_word(words):
     """prints the last word after popping it off"""
     word = words.pop(-1)
     print(word)
+
+def sort_sentence(sentence):
+    """takes in a full sentence and returns the sorted words"""
+    words = break_words(sentence)
+    return sort_words(words)
+
+def print_first_and_last(sentence):
+    """prints the first and last words of the sentence"""
+    words = break_words(sentence)
+    print_first_word(words)
+    print_last_word(words)
+
+def print_first_and_last_sorted(sentence):
+    """sorts the words then prints the first and the last one"""
+    words = sort_sentence(sentence)
+    print_first_word(words)
+    print_last_word(words)
