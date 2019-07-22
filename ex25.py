7526c4396ee3366dea52421dc7411c8291210086
petitlapin86 <skinnerpa86@yahoo.com>
2019-07-22 17:29:29 -0400

    work on ex 25

--- ex25.py
+++ ex25.py
@@ -1,1 +1,19 @@
 #
+def break_words(stuff):
+    """this function will break up words for us"""
+    words = stuff.split(' ')
+    return words
+
+def sort_words(words):
+    """sort the words"""
+    return sorted(words)
+
+def print_first_word(words):
+    """prints the first word after popping it off"""
+    word = words.pop(0)
+    print(word)
+
+def print_last_word(words):
+    """prints the last word after popping it off"""
+    word = words.pop(-1)
+    print(word)
