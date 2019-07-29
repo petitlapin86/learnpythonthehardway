95be3a2055795930c769697fed1192977ee354b8
petitlapin86 <skinnerpa86@yahoo.com>
2019-07-29 09:24:50 -0400

    more if else

--- ex30.py
+++ ex30.py
@@ -1,1 +1,24 @@
 """else and if"""
+people = 30
+cars = 40
+trucks = 14
+
+if cars > people:
+    print("we should take the cars")
+elif cars < people:
+    print("we should not take the cars")
+else:
+    print("we cant decide")
+
+
+if trucks > cars:
+    print("thats too many trucks")
+elif trucks < cars:
+    print("maybe we could take the trucks")
+else:
+    print("we still cant decide")
+
+if people > trucks:
+    print("alright lets just take the trucks")
+else:
+    print("fine, lets stay home then")
