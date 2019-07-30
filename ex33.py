6654be1343e845926d688fe90465ebabf83b5001
petitlapin86 <skinnerpa86@yahoo.com>
2019-07-30 10:32:26 -0400

    complete ex33 while loops

--- ex33.py
+++ ex33.py
@@ -1,1 +1,17 @@
 """while loops"""
+
+i = 0 #count starts at zero
+
+numbers = []  #empty list
+
+while i < 6:
+    print(f"at the top i is {i}")
+    numbers.append(i)
+
+    i = i + 1 # add one to i or i+=1
+    print("numbers now: ", numbers)
+    print(f"at the bottom i is {i}")
+
+print("the numbers: ")  #this is outside of the while loop so prints at the end
+for num in numbers:
+    print(num)
