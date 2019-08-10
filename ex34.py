d5e86636a151755b33a75097d71338a4d786687a
petitlapin86 <skinnerpa86@yahoo.com>
2019-08-10 17:04:20 -0400

    finish ex 34

--- ex34.py
+++ ex34.py
@@ -1,1 +1,7 @@
 """accessing elements of a list"""
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(animals[0] + " should be bear")
+print(animals[3] + " should be kangaroo")
+print(animals[1] + " should be python3.6")
+print(animals[5] + " should be platypus")
