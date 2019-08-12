ecd7681f9ef9043e83473d4f93482d3e03c92494
petitlapin86 <skinnerpa86@yahoo.com>
2019-08-12 13:27:50 -0400

    finish ex39.py

--- ex39.py
+++ ex39.py
@@ -12,7 +12,8 @@ states = { #4 states
 'Oregon': 'OR',
 'Florida': 'FL',
 'California' :'CA',
-'New York' : 'NY'
+'New York' : 'NY',
+'Michigan' : 'MI'
 }
 
 print(states) #should print 4 states
@@ -20,3 +21,19 @@ print(states) #should print 4 states
 states['MI'] = 'Michigan' #add a 5th state
 
 print(states) #should print 5 states
+
+
+cities = {
+'CA' : 'San Francisco',
+'MI' : 'Detroit',
+'FL': 'Jacksonville'
+}
+
+print("Michigans abbreviation is: ", states['Michigan'])
+
+print("Michigan has the City: ", cities[states['Michigan']])
+
+print("*" *10) #print a star dividing line
+
+for abbrev, city in list(cities.items()): #iterate through allm items in list
+    print(f"{abbrev} has the city {city}") #print first item and then second item 
