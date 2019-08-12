d0dc8bcccf5146a6624d8d061b792cc17816d3fc
petitlapin86 <skinnerpa86@yahoo.com>
2019-08-12 13:05:13 -0400

    finish ex 39.py

--- ex39.py
+++ ex39.py
@@ -12,8 +12,16 @@ while len(stuff) != 10: #while the length of stuff list is less than 10 run this
     next_one = more_stuff.pop() #create list next one and add an item from more_stuff to it
     print("adding: ", next_one) #display the list item added
     stuff.append(next_one) #add next_one list to stuff list
-    print(f"there are {len(stuff)} items now".) #display the number of items
+    print(f"there are {len(stuff)} items now") #display the number of items
 
 print("there we go: ", stuff)
 
 print("lets do some things with stuff")
+
+print(stuff[1]) #accesses second item on list (lists start at 0)
+print(stuff[-1]) #accesses last item on  list
+print("whoah fancy!")
+print(stuff.pop()) #pulls the last item on the list
+print(' '.join(stuff)) #joins list into one string
+
+print('#'.join(stuff[3:5])) #prints only selected list elements with a # sign between them
